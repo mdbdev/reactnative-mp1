33dfee270684f657a80bda0e567f8f2e04ebd4ad
Anya Shah <anyashah@berkeley.edu>
2023-09-27 23:02:47 -0700

    updated fa23 member pictures

--- constants/Constants.js
+++ constants/Constants.js
@@ -1,17 +1,17 @@
 const nameToPic = {
-	amolbudhiraja: ["Amol Budhiraja", require("../assets/MemberPictures/amolbudhiraja.png")],
-	amyxu: ["Amy Xu", require("../assets/MemberPictures/amyxu.png")],
-	aryanvichare: ["Aryan Vichare", require("../assets/MemberPictures/aryanvichare.jpeg")],
-	elisakim: ["Elisa Kim", require("../assets/MemberPictures/elisakim.jpeg")],
-	emilyholmes: ["Emily Holmes", require("../assets/MemberPictures/emilyholmes.jpg")],
-	ericyang: ["Eric Yang", require("../assets/MemberPictures/ericyang.png")],
-	ethankuo: ["Ethan Kuo", require("../assets/MemberPictures/ethankuo.jpg")],
-	jenniferzhao: ["Jennifer Zhao", require("../assets/MemberPictures/jenniferzhao.jpeg")],
-	josephtennyson: ["Joseph Tennyson", require("../assets/MemberPictures/josephtennyson.jpg")],
-	kevinlin: ["Kevin Lin", require("../assets/MemberPictures/kevinlin.jpg")],
-	maxinetseng: ["Maxine Tseng", require("../assets/MemberPictures/maxinetseng.jpg")],
-	nadiamohammadgholibaigi: ["Nadia Mohammad Gholi Baigi", require("../assets/MemberPictures/nadiamohammadgholibaigi.png")],
-	sameerkeswani: ["Sameer Keswani", require("../assets/MemberPictures/sameerkeswani.jpg")],
-	sriyatallapragada: ["Sriya Tallapragada", require("../assets/MemberPictures/sriyatallapragada.jpg")]
+	ambercheng: ["Amber Cheng", require("../assets/MemberPictures/ambercheng.jpg")],
+	anishsasanur: ["Anish Sasanur", require("../assets/MemberPictures/anishsasanur.png")],
+	bonniewang: ["Bonnie Wang", require("../assets/MemberPictures/bonniewang.jpg")],
+	edizertekin: ["Ediz Ertekin", require("../assets/MemberPictures/edizertekin.heic")],
+	henryjack: ["Henry Jack", require("../assets/MemberPictures/henryjack.jpeg")],
+	larachieppe: ["Lara Chieppe", require("../assets/MemberPictures/larachieppe.png")],
+	mehdikhfifi: ["Mehdi Khfifi", require("../assets/MemberPictures/mehdikhfifi.jpeg")],
+	mohamedzeidan: ["Mohamed Zeidan", require("../assets/MemberPictures/mohamedzeidan.png")],
+	sarahzong: ["Sarah Zong", require("../assets/MemberPictures/sarahzong.jpeg")],
+	satvikmuddana: ["Satvik Muddana", require("../assets/MemberPictures/satvikmuddana.jpg")],
+	siddharthganapathy: ["Siddharth Ganapathy", require("../assets/MemberPictures/siddharthganapathy.jpeg")],
+	sodbayarganbat: ["Sodbayar Ganbat", require("../assets/MemberPictures/sodbayarganbat.jpg")],
+	sriyakantipudi: ["Sriya Kantipudi", require("../assets/MemberPictures/sriyakantipudi.png")],
+	tinaye: ["Tina Ye", require("../assets/MemberPictures/tinaye.jpg")]
 }
 export { nameToPic }
